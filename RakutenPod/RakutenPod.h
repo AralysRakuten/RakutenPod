1e98d67474ede2b70af8e16985ce09f2032b98ee
Romain <romain.sanfilippo@rakuten.com>
2021-01-28 15:14:58 +0100

    Initial Commit

---
+++ RakutenPod/RakutenPod.h
@@ -0,0 +1,18 @@
+//
+//  RakutenPod.h
+//  RakutenPod
+//
+//  Created by Sanfilippo, Romain on 28/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RakutenPod.
+FOUNDATION_EXPORT double RakutenPodVersionNumber;
+
+//! Project version string for RakutenPod.
+FOUNDATION_EXPORT const unsigned char RakutenPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RakutenPod/PublicHeader.h>
+
+
